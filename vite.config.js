60eafd311875c003bbe1182b1a9a32873ec61d83
Mohit Karekar <karekar.mohit@gmail.com>
2026-08-11 23:09:48 +0200

    Rename dist

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "examples",
   build: {
-    outDir: "../examples-dist",
+    outDir: "../docs",
     emptyOutDir: true,
   },
 });
